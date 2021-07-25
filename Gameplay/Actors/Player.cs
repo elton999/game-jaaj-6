c513861ea2a74b2e29903d2b41de4f81ab4f5a25
Elton Silva <eltoncarsilva73@gmail.com>
2021-07-25 01:53:54 -0300

    jump controller

--- Gameplay/Actors/Player.cs
+++ Gameplay/Actors/Player.cs
@@ -30,13 +30,75 @@ namespace game_jaaj_6.Gameplay.Actors
         public override void Update(GameTime gameTime)
         {
             this.Scene.Camera.Target = new Vector2(this.Position.X + this.size.X / 2, this.Position.Y + this.size.Y / 2);
+            if (isGrounded)
+                this.LastPostionOnGround = this.Position;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Z))
+                this.cJump = true;
+
+            if (Keyboard.GetState().IsKeyUp(Keys.Z))
+                this.cJump = false;
+
+            this.Jump();
+
             base.Update(gameTime);
         }
 
+
+        public Vector2 LastPostionOnGround = Vector2.Zero;
         public override void UpdateData(GameTime gameTime)
         {
+            this.CheckGrounded();
+            base.UpdateData(gameTime);
+        }
 
-            //base.UpdateData(gameTime);
+        private int JumpPressedForce = 0;
+        private bool JumpPressed = false;
+        private bool JumpPressedBtn = false;
+        private float JumpForce = 500.0f;
+        private bool isGrounded = false;
+        private bool cJump = false;
+
+        public void Jump()
+        {
+            if (this.isGrounded && this.cJump)
+            {
+                this.JumpPressedBtn = true;
+                this.JumpPressed = true;
+            }
+
+            this.JumpPressed = !this.cJump ? false : this.JumpPressed;
+
+            if (this.JumpPressedBtn && this.JumpPressed && this.JumpPressedForce < 17)
+            {
+                this.velocity = new Vector2(this.velocity.X, this.JumpForce);
+                this.JumpPressedForce += 1;
+            }
+
+            if (this.isGrounded && !this.JumpPressed)
+            {
+                this.JumpPressed = false;
+                this.JumpPressedForce = 0;
+            }
+        }
+
+        public override bool isRiding(Solid solid)
+        {
+            if (solid.check(this.size, new Vector2(this.Position.X, this.Position.Y + 1)))
+                return true;
+            return false;
+        }
+
+        private void CheckGrounded()
+        {
+            this.isGrounded = false;
+            foreach (Solid solid in this.Scene.AllSolids)
+                if (solid.check(this.size, new Vector2(this.Position.X, this.Position.Y + 1)))
+                    this.isGrounded = true;
+
+            if (this.Scene.Grid.checkOverlap(this.size, new Vector2(this.Position.X, this.Position.Y + 1), this))
+                this.isGrounded = true;
+
         }
 
         public override void Draw(SpriteBatch spriteBatch)
